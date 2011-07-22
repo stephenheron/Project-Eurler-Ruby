a1a514da5b37c767a2e8c519756ce8f60e9b3ba2
Stephen Heron <stephen.heron@gmail.com>
2011-07-22 17:06:41 +0100

    Rewrote a section of code to be cleaner

--- problem_1/problem_1.rb
+++ problem_1/problem_1.rb
@@ -8,7 +8,7 @@ time do
   sum = 0
   (1..999).each do |n|
     if n % 3  == 0 or n % 5 == 0 
-      sum = sum + n
+      sum += n
     end
   end
   puts "The sum of all the multiples of 3 or 5 below 1000 is: #{sum}"
