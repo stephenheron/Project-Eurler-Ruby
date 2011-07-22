b9ce71a1ef63ce95c95da6238641166b46cf9715
Stephen Heron <stephen.heron@gmail.com>
2011-07-22 13:58:14 +0100

    Problem 1 solved

---
+++ problem_1/problem_1.rb
@@ -0,0 +1,17 @@
+def time
+  start = Time.now
+  yield
+  puts "This code took #{Time.now - start} seconds to run"
+end
+
+time do 
+  sum = 0
+  (1..999).each do |n|
+    if n % 3  == 0 or n % 5 == 0 
+      sum = sum + n
+    end
+  end
+  puts "The sum of all the multiples of 3 or 5 below 1000 is: #{sum}"
+end
+
+
